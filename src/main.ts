4e27094d018dfd726149fe5883fd003e8a26fd74
Eetu Oinonen <eetu.oinonen04@gmail.com>
2026-07-16 20:03:09 +0300

    this looks now like a dope ass hacker thingy :)

--- src/main.ts
+++ src/main.ts
@@ -3,22 +3,29 @@ import "./style.css"
 let canvas: HTMLCanvasElement
 let ctx: CanvasRenderingContext2D
 
-const fontSize = 16
-let columns = 0
-let rainDrops: number[] = []
+const cellSize = 4
+let cols = 0
+let rows = 0
 
-const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+let lastTime = 0
+let interval = 1000 / 3 // how many ms between animationframes
+let timer = 0
 
-// Make sure animation is the same speed for every screen with differing refresh rates
-const speed = 0.015
+const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789ｦｧｨｩｪｫｬｭｮｯｰｱｲｳｴｵｶｷｸｹｺｻｼｽｾｿﾀﾁﾂﾃﾄﾅﾆﾇﾈﾉﾊﾋﾌﾍﾎﾏﾐﾑﾒﾓﾔﾕﾖﾗﾘﾙﾚﾛﾜﾝ"
+
+function getRandomChar() {
+    const index = Math.floor(Math.random() * chars.length)
+    return chars[index]
+}
+
  
 function start() {
     canvas = document.querySelector<HTMLCanvasElement>("#canvas")!
     ctx = canvas.getContext("2d")!
- 
+
     resize()
  
-    requestAnimationFrame(draw)
+    requestAnimationFrame(animate)
 }
  
  
@@ -26,18 +33,29 @@ function resize() {
     canvas.width = window.innerWidth
     canvas.height = window.innerHeight
  
-    columns = Math.floor(canvas.width / fontSize)
- 
-    const rows = canvas.height / fontSize
-    rainDrops = []
-    for (let i = 0; i < columns; i++) {
-        rainDrops[i] = Math.random() * rows - rows * 0.5
-    }
+    cols = Math.floor(canvas.width / cellSize)
+    rows = Math.floor(canvas.height / cellSize)
 }
  
- 
-function draw() {
-    requestAnimationFrame(draw)
+
+function animate(timeStamp: number) {
+    const deltaTime = timeStamp - lastTime
+    lastTime = timeStamp
+    timer += deltaTime
+
+    if (timer > interval) {
+        timer = 0
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+        for (let row = 0; row < rows; row += cellSize) {
+            for (let col = 0; col < cols; col += cellSize) {
+                ctx.fillText(getRandomChar(), col * cellSize, row * cellSize)
+                ctx.textBaseline = "top"
+                ctx.fillStyle = "#00FF41"
+            } 
+        }
+    }
+    requestAnimationFrame(animate)
 }
  
  
